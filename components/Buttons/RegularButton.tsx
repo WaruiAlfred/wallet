c4e4e731a64d85665c72b2e3580a67bca52f9002
WaruiAlfred <alfredwarui17@gmail.com>
2023-01-12 23:21:28 +0300

    create Regular Button component

---
+++ components/Buttons/RegularButton.tsx
@@ -0,0 +1,35 @@
+import React, { FunctionComponent, ReactNode } from "react";
+import {
+  GestureResponderEvent,
+  StyleProp,
+  TextStyle,
+  ViewStyle,
+} from "react-native";
+import styled from "styled-components/native";
+import { colors } from "../colors";
+import RegularText from "../Texts/RegularText";
+
+const ButtonView = styled.TouchableOpacity`
+  align-items: center;
+  background-color: ${colors.primary};
+  width: 100%;
+  padding: 20px;
+  border-radius: 20px;
+`;
+
+interface ButtonProps {
+  btnStyles?: StyleProp<ViewStyle>;
+  onPress: ((event: GestureResponderEvent) => void) | undefined;
+  textStyles?: StyleProp<TextStyle>;
+  children: ReactNode;
+}
+
+const RegularButton: FunctionComponent<ButtonProps> = (props) => {
+  return (
+    <ButtonView onPress={props.onPress} style={props.btnStyles}>
+      <RegularText textStyles={props.textStyles}>{props.children}</RegularText>
+    </ButtonView>
+  );
+};
+
+export default RegularButton;
