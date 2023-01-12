dc60fa5936f06f8bf62179ff2926b6f245abf997
WaruiAlfred <alfredwarui17@gmail.com>
2023-01-12 23:21:11 +0300

    create small,regular and big text components

---
+++ components/Texts/BigText.tsx
@@ -0,0 +1,17 @@
+import { FunctionComponent } from "react";
+import styled from "styled-components/native";
+import { colors } from "../colors";
+import { TextProps } from "./types";
+
+const StyledText = styled.Text`
+  font-size: 37px;
+  color: ${colors.white};
+  text-align: left;
+  font-family: Lato-Bold;
+`;
+
+const BigText: FunctionComponent<TextProps> = (props) => {
+  return <StyledText style={props.textStyles}>{props.children}</StyledText>;
+};
+
+export default BigText;
---
+++ components/Texts/SmallText.tsx
@@ -0,0 +1,17 @@
+import { FunctionComponent } from "react";
+import styled from "styled-components/native";
+import { colors } from "../colors";
+import { TextProps } from "./types";
+
+const StyledText = styled.Text`
+  font-size: 13px;
+  color: ${colors.gray};
+  text-align: left;
+  font-family: Lato-Regular;
+`;
+
+const SmallText: FunctionComponent<TextProps> = (props) => {
+  return <StyledText style={props.textStyles}>{props.children}</StyledText>;
+};
+
+export default SmallText;
