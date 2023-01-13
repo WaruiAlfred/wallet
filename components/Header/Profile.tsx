adace0c14d1473aa23b2bbb8dda0c71d63e1fb2d
WaruiAlfred <alfredwarui17@gmail.com>
2023-01-13 14:10:58 +0300

    create Profile component

---
+++ components/Header/Profile.tsx
@@ -0,0 +1,40 @@
+import { FunctionComponent } from "react";
+import {
+  GestureResponderEvent,
+  ImageSourcePropType,
+  ImageStyle,
+  StyleProp,
+  ViewStyle,
+} from "react-native";
+import styled from "styled-components/native";
+
+const StyledView = styled.TouchableOpacity`
+  flex-direction: column;
+  height: 45px;
+  width: 45px;
+  border-radius: 15px;
+`;
+
+const StyledImage = styled.Image`
+  resize: cover;
+  width: 100%;
+  height: 100%;
+  border-radius: 15px;
+`;
+
+interface ProfileProps {
+  img: ImageSourcePropType;
+  imgStyle?: StyleProp<ImageStyle>;
+  imgContainerStyle?: StyleProp<ViewStyle>;
+  onPress?: ((event: GestureResponderEvent) => void) | undefined;
+}
+
+const Profile: FunctionComponent<ProfileProps> = (props) => {
+  return (
+    <StyledView onPress={props.onPress} style={props.imgContainerStyle}>
+      <StyledImage style={props.imgStyle} source={props.img} />
+    </StyledView>
+  );
+};
+
+export default Profile;
