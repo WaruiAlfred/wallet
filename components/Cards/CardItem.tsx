83f32c2a20079d0731cbdc68fe13133c770e0e93
WaruiAlfred <alfredwarui17@gmail.com>
2023-01-13 17:14:59 +0300

    create Card component and its types

---
+++ components/Cards/CardItem.tsx
@@ -0,0 +1,78 @@
+import { FunctionComponent } from "react";
+import styled from "styled-components/native";
+import { colors } from "../colors";
+import { ScreenWidth } from "../shared";
+import { CardProps } from "./types";
+import cardBg from "../../assets/bgs/background_transparent.png";
+import RegularText from "../Texts/RegularText";
+import { View } from "react-native";
+import SmallText from "../Texts/SmallText";
+
+const CardBackground = styled.ImageBackground`
+  height: 75%;
+  width: ${ScreenWidth * 0.67}px;
+  resize: cover;
+  background-color: ${colors.accent};
+  border-radius: 25px;
+  margin-right: 25px;
+  overflow: hidden;
+`;
+
+const CardTouchable = styled.TouchableHighlight`
+  height: 100%;
+  border-radius: 25px;
+`;
+
+const TouchableView = styled.View`
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+  flex: 1;
+`;
+
+const CardRow = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+const Logo = styled.Image`
+  width: 100%;
+  height: 80%;
+  resize: contain;
+  flex: 1;
+`;
+
+const CardItem: FunctionComponent<CardProps> = (props) => {
+  const handlePress = () => {};
+
+  return (
+    <CardBackground source={cardBg}>
+      <CardTouchable underlayColor={colors.secondary} onPress={handlePress}>
+        <TouchableView>
+          <CardRow>
+            <RegularText textStyles={{ color: colors.white }}>
+              ***{props.accountNo.slice(6, 10)}
+            </RegularText>
+          </CardRow>
+          <CardRow>
+            <View style={{ flex: 3 }}>
+              <SmallText
+                textStyles={{ marginBottom: 5, color: colors.graylight }}
+              >
+                Total Balance
+              </SmallText>
+              <RegularText textStyles={{ fontSize: 19 }}>
+                ${props.balance}
+              </RegularText>
+            </View>
+            <Logo source={props.logo} />
+          </CardRow>
+        </TouchableView>
+      </CardTouchable>
+    </CardBackground>
+  );
+};
+
+export default CardItem;
---
+++ components/Cards/CardSection.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent } from "react";
+import styled from "styled-components/native";
+import CardItem from "./CardItem";
+import { CardSectionProps } from "./types";
+
+const CardList = styled.FlatList`
+  width: 100%;
+  flex: 1;
+  padding-left: 25px;
+  padding-bottom: 15px;
+`;
+
+const CardSection: FunctionComponent<CardSectionProps> = (props) => {
+  return (
+    <CardList
+      data={props.data}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        paddingRight: 25,
+        alignItems: "center",
+      }}
+      keyExtractor={({ id }: any) => id.toString()}
+      renderItem={({ item }: any) => <CardItem {...item} />}
+    />
+  );
+};
+
+export default CardSection;
