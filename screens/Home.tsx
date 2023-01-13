6db9316e393ddeb4df5644fb852c9165cd8e91c5
WaruiAlfred <alfredwarui17@gmail.com>
2023-01-13 17:15:49 +0300

    render card & transaction section with dummy data

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -3,6 +3,10 @@ import { FunctionComponent } from "react";
 import styled from "styled-components/native";
 import { colors } from "../components/colors";
 import { Container } from "../components/shared";
+import logo1 from "../assets/cards/mc.png";
+import logo2 from "../assets/cards/visa_white.png";
+import CardSection from "../components/Cards/CardSection";
+import TransactionSection from "../components/Transactions/TransactionSection";
 
 const HomeContainer = styled(Container)`
   background-color: ${colors.graylight};
@@ -11,9 +15,71 @@ const HomeContainer = styled(Container)`
 `;
 
 const Home: FunctionComponent = () => {
+  const cardsData = [
+    {
+      id: 1,
+      accountNo: "2343345454646",
+      balance: 2000.22,
+      alias: "Work Debit",
+      logo: logo1,
+    },
+    {
+      id: 2,
+      accountNo: "23433656454646",
+      balance: 23400.22,
+      alias: "Personal Prepaid",
+      logo: logo2,
+    },
+    {
+      id: 3,
+      accountNo: "23876754646",
+      balance: 454000.22,
+      alias: "School Prepaid",
+      logo: logo1,
+    },
+  ];
+
+  const transactionData = [
+    {
+      id: 1,
+      amount: "-$86.00",
+      date: "14 Sep 2021",
+      title: "Taxi",
+      subtitle: "Uber Car",
+      art: {
+        background: colors.primary,
+        icon: "car",
+      },
+    },
+    {
+      id: 2,
+      amount: "-$286.00",
+      date: "14 Sep 2021",
+      title: "Shopping",
+      subtitle: "Ali Express",
+      art: {
+        background: colors.tertiary,
+        icon: "cart",
+      },
+    },
+    {
+      id: 3,
+      amount: "-$586.00",
+      date: "14 Sep 2021",
+      title: "Travel",
+      subtitle: "Emirates",
+      art: {
+        background: colors.accent,
+        icon: "airplane",
+      },
+    },
+  ];
+
   return (
     <HomeContainer>
       <StatusBar style="dark" />
+      <CardSection data={cardsData} />
+      <TransactionSection data={transactionData} />
     </HomeContainer>
   );
 };
