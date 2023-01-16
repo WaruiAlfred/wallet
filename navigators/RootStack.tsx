dbcb35aedb0dc5ac94a79ed28608424013a02471
WaruiAlfred <alfredwarui17@gmail.com>
2023-01-17 01:30:48 +0300

    register Balance screen under screens

--- navigators/RootStack.tsx
+++ navigators/RootStack.tsx
@@ -7,10 +7,14 @@ import Home from "../screens/Home";
 import Welcome from "../screens/Welcome";
 import Avi from "../assets/avi/avatar.png";
 import Profile from "../components/Header/Profile";
+import { CardProps } from "../components/Cards/types";
+import Balance from "../screens/Balance";
+import { Ionicons } from "@expo/vector-icons";
 
 export type RootStackParamList = {
   Welcome: undefined;
   Home: undefined;
+  Balance: CardProps;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -44,7 +48,7 @@ const RootStack: FunctionComponent = () => {
             />
           ),
         }}
-        initialRouteName="Home"
+        initialRouteName="Welcome"
       >
         <Stack.Screen
           name="Welcome"
@@ -63,6 +67,25 @@ const RootStack: FunctionComponent = () => {
               />
             ),
           }}
+        />
+        <Stack.Screen
+          name="Balance"
+          component={Balance}
+          options={({ route }) => ({
+            headerTitle: route?.params?.alias,
+            headerTitleAlign: "center",
+            headerBackImage: (props) => (
+              <Ionicons
+                {...props}
+                name="chevron-back"
+                size={25}
+                color={colors.secondary}
+              />
+            ),
+            headerLeftContainerStyle: {
+              paddingLeft: 0,
+            },
+          })}
         />
       </Stack.Navigator>
     </NavigationContainer>
